c08ec2e5fa6028311185b43d37d6fadd3893c4c3
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 01:39:37 -0300

    Register phase 01 foundation seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Seeders de desenvolvimento serão adicionados por módulo.
+        $this->call(FoundationSeeder::class);
     }
 }
