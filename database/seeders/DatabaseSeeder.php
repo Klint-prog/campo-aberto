4bf2b3a3e7e248d5e160bfb4b924904643a99229
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-04 23:30:04 -0300

    chore: add database seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Seeders de desenvolvimento serão adicionados por módulo.
+    }
+}
