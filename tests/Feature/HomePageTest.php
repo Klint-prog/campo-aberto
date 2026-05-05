3b288fb720607bb4792fb55c6e6f4ee47ac34e77
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-04 23:31:30 -0300

    test: add home page feature test

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    public function test_home_page_loads_successfully(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+        $response->assertSee('Campo Aberto Tecnologia Rural');
+    }
+}
