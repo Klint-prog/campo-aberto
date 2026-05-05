d0d3b1bf9631f95af2566125fb14138df98ce146
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 04:10:45 -0300

    fix auth register login rate limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,8 +6,12 @@ use App\Models\Farm;
 use App\Models\User;
 use App\Policies\FarmPolicy;
 use App\Policies\UserPolicy;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        RateLimiter::for('login', function (Request $request) {
+            $email = Str::lower((string) $request->input('email'));
+
+            return Limit::perMinute(5)->by($email.'|'.$request->ip());
+        });
+
         Gate::policy(User::class, UserPolicy::class);
         Gate::policy(Farm::class, FarmPolicy::class);
 
