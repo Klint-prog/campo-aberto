5a8a1e7b8abf24a1b5970fed7fd9f4f8657b8db9
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 03:42:26 -0300

    chore(auth): keep database seeder empty for phase 02

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(FoundationSeeder::class);
+        // Seeders de dados de demonstração serão reativados após estabilização das permissões.
     }
 }
