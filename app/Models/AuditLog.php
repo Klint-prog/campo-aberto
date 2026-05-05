190e23ccc12f167ace5a2690a2873ad92c10488b
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 17:09:09 -0300

    Use default audit log primary key strategy

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -2,17 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class AuditLog extends Model
 {
-    use HasUuids;
-
-    public $incrementing = false;
-
-    protected $keyType = 'string';
-
     public $timestamps = false;
 
     protected $fillable = [
