42ea334f516669b2dca9e0117be76be27845f2e4
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 03:27:25 -0300

    fix(auth): use default guard for internal api

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('internal/v1')
     ->as('api.internal.v1.')
-    ->middleware(['auth:sanctum', 'tenant.scope'])
+    ->middleware(['auth', 'tenant.scope'])
     ->group(function (): void {
         Route::get('/me', MeController::class)->name('me');
         Route::get('/tenants/current', CurrentTenantController::class)->name('tenants.current');
