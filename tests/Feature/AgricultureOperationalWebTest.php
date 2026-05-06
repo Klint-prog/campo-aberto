cd0a6fac5ca1a3e20b3becea764fd0da639acf92
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-06 00:41:24 -0300

    Fix agriculture operational web tests CSRF handling

--- tests/Feature/AgricultureOperationalWebTest.php
+++ tests/Feature/AgricultureOperationalWebTest.php
@@ -8,6 +8,7 @@ use App\Models\Farm;
 use App\Models\Harvest;
 use App\Models\Tenant;
 use App\Models\User;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
@@ -15,6 +16,13 @@ use Tests\TestCase;
 class AgricultureOperationalWebTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
 
     public function test_user_can_plan_activity_from_web_interface(): void
     {
