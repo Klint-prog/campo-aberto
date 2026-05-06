f5156daa3727ce184f3ba35ef9f551cde0a70475
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 23:58:30 -0300

    Add pasture web controller

---
+++ app/Http/Controllers/PastureWebController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PastureWebController extends MasterDataWebController
+{
+    protected function module(): string
+    {
+        return 'pastures';
+    }
+}
