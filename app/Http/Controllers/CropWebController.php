6571fa2cf871cbe3e028e03f2d9f3912b251f474
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 23:58:42 -0300

    Add crop web controller

---
+++ app/Http/Controllers/CropWebController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CropWebController extends MasterDataWebController
+{
+    protected function module(): string
+    {
+        return 'crops';
+    }
+}
