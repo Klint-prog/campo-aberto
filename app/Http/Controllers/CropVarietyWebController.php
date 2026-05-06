3747a01410cb3690b0f58c0e81c3676fa1e7b500
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 23:58:56 -0300

    Add crop variety web controller

---
+++ app/Http/Controllers/CropVarietyWebController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CropVarietyWebController extends MasterDataWebController
+{
+    protected function module(): string
+    {
+        return 'crop-varieties';
+    }
+}
