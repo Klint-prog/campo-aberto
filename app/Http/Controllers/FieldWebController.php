d5da114f91b733f4f66c050950b25b2a22c2eed9
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 23:57:50 -0300

    Add field web controller

---
+++ app/Http/Controllers/FieldWebController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FieldWebController extends MasterDataWebController
+{
+    protected function module(): string
+    {
+        return 'fields';
+    }
+}
