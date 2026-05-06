1f69882a6a59d95ccba6a6fa7ebc044dc648b116
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-05 23:59:08 -0300

    Add season web controller

---
+++ app/Http/Controllers/SeasonWebController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeasonWebController extends MasterDataWebController
+{
+    protected function module(): string
+    {
+        return 'seasons';
+    }
+}
